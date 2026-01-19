da18ccda0b7c0888382ca6f75bc76d380c4d061e
DCT-Berinyuy <verlaberinyuy8@gmail.com>
2026-01-19 23:44:43 +0100

    Hero Section

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/widgets/hero_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Welcome to the Home Page!'),
-    );
+    return Padding(padding: EdgeInsets.all(20.0),
+    child: Column(
+      children: [
+        HeroWidget(),
+      ],
+    ),);
   }
 }
